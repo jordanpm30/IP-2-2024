0145dccc43b65c0086a5f1a81b80301b1438bd2d
jordanpm30 <jordanpm30@gmail.com>
2024-09-04 00:21:24 -0300

    nose

--- instrucciones.hs
+++ instrucciones.hs
@@ -2,10 +2,35 @@
 {-
 pasos negativos osea nada mas una vez 
 zero    =  llamo a git con                                                                 = git init
-zero 2  =  ahora lo que tengo que hacer es llamar al repositor el mio ej es el siguiente   = git 
+zero 2  =  ahora lo que tengo que hacer es llamar al repositor el mio ej es el siguiente   = git remote add origin https://github.com/jordanpm30/IP-2-2024.git
 primero = verifico que es lo que falta agregar con                                         = git status 
 segundo = agrego lo que anadi como carpetas o algo con                                     = git add . (es para agregar todo o habia otro para hacerlo individual )
+    Bis =                                                                                  = git add <nombre-del-archivo>
 tercero = verifico si lo agrege bien con el                                                = git status 
 cuarto  = aca lo que tengo que hacer es lo ultimo que hize para facilitarlo al leyente con = git commit -m "nombre de lo que hiziste"
 quinto  = finalizo osea termine y lo subo con                                              = git push
+
+Resumen de Comandos
+Para descartar cambios en un archivo específico:
+bash
+Copiar código
+git checkout -- instrucciones.hs
+
+Para añadir el archivo al área de preparación:
+bash
+Copiar código
+git add instrucciones.hs
+
+Para confirmar cambios:
+bash
+Copiar código
+git commit -m "Mensaje del commit"
+
+Para descartar todos los cambios no confirmados:
+bash
+Copiar código
+git reset --hard
+
+si esta M (AMARRELLO) es porque lo puedo actualizar 
+SI esta U (NO PASA NADA )  CREO 
 -}
