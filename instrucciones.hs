c2ee123721012e32d9cde292f07da56c5481e8e7
jordanpm30 <jordanpm30@gmail.com>
2024-09-04 00:06:16 -0300

    instrucciones

---
+++ instrucciones.hs
@@ -0,0 +1,8 @@
+--https://www.youtube.com/watch?v=44ziZ12rJwU&t=94s
+{-
+primero = verifico que es lo que falta agregar con                                         = git status 
+segundo = agrego lo que anadi como carpetas o algo con                                     = git add . (es para agregar todo o habia otro para hacerlo individual )
+tercero = verifico si lo agrege bien con el                                                = git status 
+cuarto  = aca lo que tengo que hacer es lo ultimo que hize para facilitarlo al leyente con = git commit -m "nombre de lo que hiziste"
+quinto  = finalizo osea termine y lo subo con                                              = git push
+-}
