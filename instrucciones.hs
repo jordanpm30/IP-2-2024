e8b02b14c80d0ddf262e7b8778556f12423d6695
jordanpm30 <jordanpm30@gmail.com>
2024-09-04 00:10:54 -0300

    instrucciones

--- instrucciones.hs
+++ instrucciones.hs
@@ -1,5 +1,8 @@
 --https://www.youtube.com/watch?v=44ziZ12rJwU&t=94s
 {-
+pasos negativos osea nada mas una vez 
+zero    =  llamo a git con                                                                 = git init
+zero 2  =  ahora lo que tengo que hacer es llamar al repositor el mio ej es el siguiente   = git 
 primero = verifico que es lo que falta agregar con                                         = git status 
 segundo = agrego lo que anadi como carpetas o algo con                                     = git add . (es para agregar todo o habia otro para hacerlo individual )
 tercero = verifico si lo agrege bien con el                                                = git status 
